d17ef870d5792dc9276be100c78648d77b06ec43
Matheus Marc Guichard <matheusmarcguichard@gmai.comm>
2025-12-02 22:40:15 -0300

    nunca mais uso --force

--- day1/day1ProblemPart1.js
+++ day1/day1ProblemPart1.js
@@ -3,33 +3,18 @@ const path = require('path');
 const filePath = path.join(__dirname, 'day1.txt');
 const fileContent = fs.readFileSync(filePath, 'utf-8');
 const values = fileContent.split('\n').filter(line => line.trim() !== '');
-const adjustValues = (arr) => {return arr.map(e => e.includes('L')? e.replace(/\D/g, "") * -1: e.replace(/\D/g, "") * 1);}
+const adjustValues = (arr) => { return arr.map(e => e.includes('L')? e.replace(/\D/g, "") * -1: e.replace(/\D/g, "") * 1);}
 
 
 const ciclicalGroup = (arr, start) => {
     let acc = start;
-    let sum = 0;
-
-    for(let i = 0; i < arr.length; i++) {
-        const e = arr[i];
-
-        if(e > 0) {
-            for(let j = 0; j < e; j++) {
-                acc = (acc + 1) % 100;
-                if(acc === 0) sum++;
-            }
-        } else {
-            for(let j = 0; j < -e; j++) {
-                acc = (acc - 1 + 100) % 100;
-                if(acc === 0) sum++;
-            }
-        }
-    }
-
-    return sum;
+    return arr.map(e => {
+        acc = (acc + e + 100) % 100;
+        return acc;
+    });
 };
 
-const senha = ciclicalGroup(adjustValues(values), 50);
+
+const senha = ciclicalGroup(adjustValues(values), 50).filter(b => b ===0).length;
 
 console.log(senha);
-
